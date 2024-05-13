fdc559fff71e1ab0767b53f639f10380e469af95
smitens <zane.smite@gmail.com>
2024-05-13 14:46:36 +0300

    JSON file and code refactored.

--- NarvesenStore.php
+++ NarvesenStore.php
@@ -1,104 +1,85 @@
 <?php
 
-function selectProduct($products): stdClass
+function displayProducts($products): void
 {
-    $selectedProducts = new stdClass();
+    $groupedProducts = [];
+    foreach ($products as $product) {
+        $category = $product->category;
+        if (!isset($groupedProducts[$category])) {
+            $groupedProducts[$category] = [];
+        }
+        $groupedProducts[$category][] = $product;
+    }
 
-    echo "Welcome to our store!\nAvailable products:\n";
+    echo "Available products:\n";
     $index = 1;
-    foreach ($products -> products as $productType => $productList) {
-        echo ($productType) . ":\n";
-        foreach ($productList as $product) {
-            echo $index . " " . $product -> name . ", $" . number_format($product -> price / 100, 2) . "\n";
+    foreach ($groupedProducts as $category => $products) {
+        echo "$category: \n";
+        foreach ($products as $product) {
+            echo $index . " " . $product->name . ", EUR " . number_format($product->price / 100, 2) . "\n";
             $index++;
         }
     }
+}
 
-    while (true) {
-        echo "Enter the number of the product you want to select (type 'stop' to finish): ";
-        $choice = readline();
+$products = json_decode(file_get_contents('products.json'));
+$cart = [];
 
-        if ($choice === 'stop') {
+echo "Welcome to our store!\n";
+
+while (true) {
+
+    displayProducts($products);
+
+    $choice = readline("Enter the number of the product you want to select (type 'stop' to finish): ");
+    if ($choice === 'stop') {
+        if (empty($cart)) {
+            echo "Your cart is empty. Do you want to continue shopping?\n";
+            $continueShopping = readline("Enter 'yes' to continue or 'no' to finish: ");
+            if ($continueShopping === 'yes') {
+                continue;
+            } else {
+                echo "Looking forward to meet you again ir our shop!\n";
+                break;
+            }
+        } else {
             echo "Selection done.\n";
             break;
         }
-
-        $choice = intval($choice);
-        if (!is_numeric($choice) || $choice < 1 || $choice > $index - 1) {
-            echo "Invalid choice. Please enter a valid number or 'stop' to finish.\n";
-            continue;
-        }
-
-        $selectedProduct = null;
-        $currentIndex = 1;
-        foreach ($products -> products as $productList) {
-            foreach ($productList as $product) {
-                if ($currentIndex == $choice) {
-                    $selectedProduct = $product;
-                    break 2;
-                }
-                $currentIndex++;
-            }
-        }
-
-        if (!empty($selectedProduct)) {
-            while (true) {
-                echo "Enter the amount of " . $selectedProduct -> name . ": ";
-                $amount = intval(readline());
-
-                if ($amount <= 0) {
-                    echo "Invalid amount. Please enter a positive number.\n";
-                } else {
-                    $totalPrice = number_format($selectedProduct -> price / 100 * $amount, 2);
-                    echo "Selected: " . $selectedProduct -> name . ", Amount: " . $amount . ", Total Price: EUR " . $totalPrice . "\n";
-                    $selectedProducts->{$selectedProduct->name} = ['name' => $selectedProduct->name, 'price' => $selectedProduct->price, 'amount' => $amount];;
-                    break;
-                }
-            }
+    } elseif (!is_numeric($choice) || $choice < 1 || $choice > count($products)) {
+        echo "Invalid choice. Please enter a valid number or 'stop' to finish.\n";
+    } else {
+        $product = $products[$choice - 1];
+        echo "Enter the amount of " . $product->name . ": ";
+        $amount = intval(readline());
+        if ($amount <= 0) {
+            echo "Invalid amount. Please enter a positive number.\n";
         } else {
-            echo "Invalid choice. Please enter a valid number or 'stop' to finish.\n";
+            $item = new stdClass();
+            $item -> product = $product;
+            $item -> amount = $amount;
+            $cart [] = $item;
+            $totalProductPrice = number_format($product->price / 100 * $amount, 2);
+            echo "Selected: " . $product->name . ", Amount: " . $amount . ", Total Price: EUR " . $totalProductPrice . "\n";
         }
     }
-
-    return $selectedProducts;
 }
-$products = json_decode(file_get_contents('products.json'), false);
-$selectedProducts = selectProduct($products);
 
 echo "Your shopping cart contains the following items:\n";
 $totalPrice = 0;
-$totalItems = 0;
-foreach ($selectedProducts as $productName => $selectedProduct) {
-        $totalPrice += $selectedProduct ['price'] / 100 * $selectedProduct ['amount'];
-        $totalItems += $selectedProduct ['amount'];
-        echo "- " . $selectedProduct ['name'] . ", " . $selectedProduct ['amount'] . " pcs - EUR " .
-            number_format($selectedProduct ['price'] / 100 * $selectedProduct ['amount'], 2) . "\n";
+foreach ($cart as $item) {
+    $totalPrice += $item->product->price / 100 * $item->amount;
+    echo "- " . $item->product->name . ", " . $item->amount . " pcs - EUR " .
+        number_format($item->product->price / 100 * $item->amount, 2) . "\n";
 }
+$totalItems = array_sum(array_column($cart, 'amount'));
 echo "Total items:" . $totalItems . "\n";
 echo "Total price: EUR " . number_format($totalPrice, 2) . "\n";
 
-
-function confirmPurchase(): bool
-{
-    while (true) {
-    echo "Do you want to proceed with the purchase? (yes/no): ";
+echo "Do you want to proceed with the purchase? (yes/no): ";
     $choice = strtolower(readline());
-        if ($choice === 'yes') {
-            return true;
-        } elseif ($choice === 'no') {
-            return false;
-        } else {
-            echo "Invalid value. Please enter 'yes' or 'no'.\n";
-        }
-    }
-}
-
-if (!empty((array)$selectedProducts)) {
-    if (confirmPurchase()) {
+    if ($choice === 'yes') {
         echo "Thank you for your purchase! Looking forward to see you again soon!\n";
     } else {
-        echo "Purchase cancelled.\n";
+        echo "Purchase has been canceled.\n";
     }
-} else {
-    echo "No items selected. Exiting store.\n";
-}
